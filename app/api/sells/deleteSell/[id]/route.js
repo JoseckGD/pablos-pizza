88c7c008eac74018fa6fbde67a1518cb6d1dc7af
MTKArmandoGP <jsearmando767@micorreo.upp.edu.mx>
2023-07-07 13:09:06 -0600

    DELETE y PUT ventas

---
+++ app/api/sells/deleteSell/[id]/route.js
@@ -0,0 +1,53 @@
+import { NextResponse } from "next/server";
+import dbPizzeriaConnection from '../../../../../utils/dbPizzeriaConnection';
+
+export async function DELETE(req,{params}){
+
+    const {id}= await params
+    try {
+        const sell = await getSellByID(parseInt(id))
+        console.log(sell);
+        if (sell.length > 0) {
+            console.log(await deleteSell(parseInt(id)));
+            return NextResponse.json({ status: true, message: 'Venta eliminada exitosamente' });
+        } else {
+            return NextResponse.json({ status: false, message: 'No existe el registro de esa venta' });
+        }
+    } catch (error) {
+        return NextResponse.json({ status: false, message: 'Error al eliminar el usuario: ', error: error });
+    }
+
+}
+
+
+async function getSellByID(id){
+
+    return new Promise((resolve, reject) => {
+        const query = 'SELECT * FROM ventas WHERE id = ?';
+        const values = [id];
+        dbPizzeriaConnection.query(query, values, (error, results) => {
+            if (error) {
+                reject(error);
+                return;
+            }
+            resolve(results);
+        });
+    });
+
+}
+
+async function deleteSell(id){
+
+    return new Promise((resolve, reject) => {
+        const query = 'DELETE FROM ventas WHERE id = ?';
+        const values = [id];
+        dbPizzeriaConnection.query(query, values, (error, results) => {
+            if (error) {
+                reject(error);
+                return;
+            }
+            resolve(true);
+        });
+    });
+
+}
---
+++ app/api/sells/modifySell/route.js
@@ -0,0 +1,31 @@
+import { NextResponse } from 'next/server';
+import dbPizzeriaConnection from '../../../../utils/dbPizzeriaConnection';
+
+
+export async function PUT(req, res) {
+    const body = await req.json()
+    try {
+        if (await editSell(body)) {
+            return NextResponse.json({ status: true, message: 'Venta actualizada exitosamente' });
+        }
+        return NextResponse.json({ status: true, message: 'No existe ese registro de venta' });
+    } catch (error) {
+        return NextResponse.json({ status: false, message: 'Error al actualizar la venta: ', error: error });
+    }
+}
+
+async function editSell(data) {
+    console.log(data);
+    const { usuario_id, fecha, total,id } = data
+    return new Promise((resolve, reject) => {
+        const query = 'UPDATE ventas SET usuario_id = ?, fecha = ?, total = ? WHERE id = ?';
+        const values = [usuario_id, fecha, total, id];
+        dbPizzeriaConnection.query(query, values, (error, results) => {
+            if (error) {
+                reject(error);
+                return;
+            }
+            resolve(true);
+        });
+    });
+}
